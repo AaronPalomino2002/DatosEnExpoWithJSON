fd527e964194b87ced16fc861ecbe7b3aea48eb0
AaronPalomino2002 <palominofalconaaron@gmail.com>
2024-10-19 01:12:29 -0500

    Elaborando un listado, usando un archivo DATAJSON

---
+++ App.js
@@ -0,0 +1,67 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Svg, { Path, Ellipse } from "react-native-svg";
+const { width, height } = Dimensions.get('window');
+import ButtonGradient from './ButtonGradient';
+import Login from './src/Login';
+import Home from './components/Home';
+import Register from './src/Register';
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle={"light-content"} backgroundColor={"#054169"} />
+      <NavigationContainer >
+        <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} ></Stack.Screen>
+            <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} ></Stack.Screen>
+            <Stack.Screen name='Register' component={Register} options={{ headerShown: false }} ></Stack.Screen>
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: "#f1f1f1",
+    flex: 1,
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerSvg: {
+    width: width,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 80,
+    color: "#34434D",
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  Textinput: {
+    padding: 10,
+    paddingStart: 20,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff'
+  },
+  forgotPassword: {
+    fontSize: 14,
+    color: 'gray',
+    marginTop: 20
+  }
+});
---
+++ ButtonGradient.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from "@react-navigation/native";
+
+
+
+export default function ButtonGradient(){
+    const navigation = useNavigation()
+    return(
+        <TouchableOpacity style={styles.container} onPress={() => navigation.navigate("Home")}>
+            <LinearGradient
+                // Button Linear Gradient
+                colors={['#FFB677', '#FF3CBD']}
+                start={{x:1, y:0}}
+                end={{x:0, y: 1}}
+                style={styles.button}
+                >
+                <Text style={styles.text}>Sign in</Text>
+            </LinearGradient>
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        alignItems: 'center',
+        width: 200,
+        marginTop: 50,
+    },
+    text:{
+        fontSize: 14,
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    button:{
+        width: '80%',
+        height: 50,
+        borderRadius: 25,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
